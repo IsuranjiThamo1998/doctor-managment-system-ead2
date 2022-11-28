03afab2033c14c959696a63e1cca646031154b97
IsuranjiThamo1998 <KGIThamosha98@gmail.com>
2022-11-28 12:36:40 +0530

    Add all my files

---
+++ src/hospital/managment/DbUtils.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hospital.managment;
+
+import java.sql.ResultSet;
+import javax.swing.table.TableModel;
+
+/**
+ *
+ * @author Isuranji Thamosha
+ */
+class DbUtils {
+
+    static TableModel resultSetToTableModel1(ResultSet rs) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
